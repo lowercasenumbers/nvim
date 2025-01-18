883da90e6fc8af5640d911d1984454938a2b6a62
mike <mike@pop-os.local>
2025-01-18 13:54:23 -0500

    initial commit

---
+++ lua/mike/init.lua
@@ -0,0 +1,4 @@
+require("mike.remap")
+require("mike.set")
+require("mike.lazy_init")
+
---
+++ lua/mike/set.lua
@@ -0,0 +1,19 @@
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+---vim.opt.colorcolumn = "80"
