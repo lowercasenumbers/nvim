46491e652daf8573e07424fa8cd39361082d6e3c
mike <mike@pop-os.local>
2025-01-18 14:27:19 -0500

    Add highlight on yank

--- lua/mike/init.lua
+++ lua/mike/init.lua
@@ -2,3 +2,31 @@ require("mike.remap")
 require("mike.set")
 require("mike.lazy_init")
 
+
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+vim.filetype.add({
+    extension = {
+        templ = 'templ',
+    }
+})
+
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
