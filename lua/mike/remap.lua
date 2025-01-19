9842726b857384ede4961f2d7142ecb958a2a9c8
mike <mike@pop-os.local>
2025-01-18 22:02:00 -0500

    Add yank to system clipboard

--- lua/mike/remap.lua
+++ lua/mike/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+--- Copy to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+Y]])
+
