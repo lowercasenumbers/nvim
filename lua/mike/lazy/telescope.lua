fec77d98547763cb524586336e604a560380467a
Mike Vedete <mvedete@gmail.com>
2025-02-16 15:46:16 -0500

    change fuzy finder to ff and fg

--- lua/mike/lazy/telescope.lua
+++ lua/mike/lazy/telescope.lua
@@ -11,9 +11,9 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>fw', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
