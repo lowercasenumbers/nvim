2a19a8ac5027fcc6c0994b769d07fe146382523b
Mike <mvedete@gmail.com>
2025-06-07 08:44:03 -0400

    jk to escape to normal

--- lua/mike/remap.lua
+++ lua/mike/remap.lua
@@ -4,4 +4,4 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 --- Copy to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+Y]])
-
+vim.keymap.set("i","jk", "<ESC>", {noremap=ture})
